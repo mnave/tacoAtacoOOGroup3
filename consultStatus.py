9c1483db94a194afc0237c4bade1ae93235729f5
mnave <mariana.nave@gmail.com>
2016-03-07 20:58:14 +0000

    updated to include 1st working period (0911)

--- consultStatus.py
+++ consultStatus.py
@@ -13,6 +13,8 @@ from Vehicle import Vehicle
 from Reservation import Reservation
 from Service import Service
 from ServicesList import ServicesList
+from operator import attrgetter
+
 
 
 # DOCUMENTATION NOT UPDATED
@@ -202,8 +204,8 @@ def waiting4ServicesList(drivers_p, vehicles_p, services_p):
 
     # Sorting according to increasing availability time,
     # untying with drivers's names
-    detailedWaitingList = sorted(detailedWaitingList)
 
+    detailedWaitingList = sorted(detailedWaitingList)
     return detailedWaitingList
 
 # FOR TESTING
@@ -215,3 +217,38 @@ def waiting4ServicesList(drivers_p, vehicles_p, services_p):
 #
 # for i in w:
 #     print i.getServiceDriver()
+
+
+def emptyServices(drivers,vehicles):
+    '''
+    Requires: drivers is a dict with a structure as in the output
+    of readDriversFile; vehicles is a dict with the structure as in
+    the output of readVehiclesFile.
+    Ensures: A list of services regarding the first working period (0911);
+    vehicles, lexicographic sorted by plate, are attributed to drivers
+    according to their entry hour. All the service-related information is
+    set as a "no service" (_no_client_, _no_circuit_, service kms = 0), Arrival and Departure
+    hours are set as the Driver's entry hour and being ready to work,
+    drivers' status is standby, of course!
+    '''
+
+
+    lstDrivers = []
+    lstService = ServicesList()
+    for i in drivers:
+        lstDrivers.append(drivers[i])
+
+    #sort drivers for the 1st period: 0911
+    d = sorted(lstDrivers, key=attrgetter("_entryHour"))
+    v = sorted(vehicles)
+
+    for i in range(len(d)):
+        driverName = d[i].getDriverName()
+        vehiclePlate = v[i]
+        drv = d[i]
+        serv = Service(driverName,vehiclePlate,"",drv.getDriverEntryHour(),drv.getDriverEntryHour(),"","","")
+        serv.noService()
+        lstService.append(serv)
+    return lstService
+
+
