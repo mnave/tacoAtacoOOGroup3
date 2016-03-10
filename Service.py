1e5b39eb90bdfb3ecf42db25ece348fb7153b15d
LLCampos <luis1212@sapo.pt>
2016-03-10 19:34:16 +0000

    update Service
    considering the new DetailedService class

--- Service.py
+++ Service.py
@@ -46,15 +46,6 @@ class Service(object):
     def getServiceDriverStatus(self):
         return self._servDriverStatus
 
-    def getAccumTime(self):
-        return self._accumTime
-
-    def getVehicleKmsDone(self):
-        return self._vehicleKmsDone
-
-    def getVehicleAutonomy(self):
-        return self._vehicleAutonomy
-
     def setServiceDriver(self, servDriver):
         self._servDriver = servDriver
 
@@ -79,35 +70,10 @@ class Service(object):
     def setServiceDriverStatus(self, servDriverStatus):
         self._servDriverStatus = servDriverStatus
 
-    def setAccumTime(self, AccumTime):
-        self._accumTime = AccumTime
-
-    def setVehicleAutonomy(self, vehicAutonomy):
-        self._vehicleAutonomy = vehicAutonomy
-
-    def setVehicleKmsDone(self,  kmsDone):
-        self._vehicleKmsDone = kmsDone
-
-    def setDetailedInformation(self, driver, vehicle):
-        self._accumTime = driver.getDriverAccumTime()
-        self._vehicleKmsDone = vehicle.getVehicleKmsDone()
-        self._vehicleAutonomy = vehicle.getVehicleAutonomy()
-
     def resetVehic(self):
         """Changes the status of a driver/vehicle to 'standby'."""
 
         self.setServiceDriverStatus(STATUSStandBy)
-
-    def afterCharge(self):
-        """Updates a service to a after charge status."""
-
-        self.setServiceClient(NOCLIENT)
-        self.setServiceArrivalHour(self.getServiceArrivalHour().add(Time("01:00")))
-        self.setServiceDepartHour(self.getServiceArrivalHour())
-        self.setServiceCircuit(NOCIRCUIT)
-        self.setServiceCircuitKms("0")
-        self.setServiceDriverStatus(STATUSStandBy)
-        self.setVehicleKmsDone("0")
 
     def noService(self):
         """Update a service's list when there is no service."""
@@ -116,54 +82,6 @@ class Service(object):
         self.setServiceCircuit(NOCIRCUIT)
         self.setServiceCircuitKms("0")
         self.setServiceDriverStatus(STATUSStandBy)
-
-    def calculateKmsLeft(self):
-        """Calculates how many kilometers the vehicle of this service can still do."""
-
-        return int(self.getVehicleAutonomy()) - int(self.getVehicleKmsDone())
-
-    def updateOneService(self, reservation):
-        """Assign a driver with her vehicle to a service that was reserved.
-
-        Requires:
-        reservation is a Reservation object.
-        Ensures:
-        self attributes get updated considering the new reservation.
-        """
-        # Adds information to the new service
-        self.setServiceClient(reservation.getReservClient())
-
-        # checks if it's going to be a delay, that is, if the driver/vehicle is not available at the requested time
-        self.calculateNewStartAndEndHour(reservation)
-
-        self.setServiceCircuit(reservation.getReservCircuit())
-        self.setServiceCircuitKms(reservation.getReservCircuitKms())
-
-        # Calculates how much work time is left for the driver after this service
-        duration = reservation.duration()
-        new_accumulated_hours = self.getAccumTime().add(duration)
-        allowed_time_left = Time(TIMELimit).diff(new_accumulated_hours)
-
-        # Calculates how much kms are left fot the vehicle after this service
-        new_accumulated_kms = int(self.getVehicleKmsDone()) + int(self.getServiceCircuitKms())
-        allowed_kms_left = int(self.getVehicleAutonomy()) - new_accumulated_kms
-
-        # set common parameters
-        self.setAccumTime(new_accumulated_hours)
-        self.setVehicleKmsDone(new_accumulated_kms)
-
-        # Adds the rest of the information, depending on the allowed time and kms left
-        if allowed_time_left < Time(TIMEThreshold):
-            self.setServiceDriverStatus(STATUSTerminated)
-
-        elif allowed_kms_left < AUTONThreshold:
-            self.setServiceDriverStatus(STATUSCharging)
-            self.setServiceCircuitKms(reservation.getReservCircuitKms())
-
-        else:
-            self.setServiceDriverStatus(STATUSStandBy)
-
-        self.setVehicleAutonomy(self.getVehicleAutonomy())
 
     def calculateNewStartAndEndHour(self, reservation):
         """Updates the Service starting and end time considering the new reservation.
@@ -198,21 +116,6 @@ class Service(object):
         elif self.getServiceArrivalHour() > other_detailedService.getServiceArrivalHour():
             return False
         else:
-
-            # compare by accumulated time
-            if hasattr(self, "_accumTime"):
-                if self.getAccumTime() < other_detailedService.getAccumTime():
-                    return True
-                elif self.getAccumTime() > other_detailedService.getAccumTime():
-                    return False
-                else:
-                    # compare by driver name
-                    if self.getServiceDriver() < other_detailedService.getServiceDriver():
-                        return True
-                    else:
-                        return False
-            else:
-                # compare by driver name
                 if self.getServiceDriver() < other_detailedService.getServiceDriver():
                     return True
                 else:
