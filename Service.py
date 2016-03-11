942c92ea7ba62e8bfe7f3aabe2ce9bdfee8c4ddd
LLCampos <luis1212@sapo.pt>
2016-03-11 17:22:19 +0000

    add documentation to Service

--- Service.py
+++ Service.py
@@ -11,8 +11,20 @@ from Time import Time
 
 
 class Service(object):
+    """A Taca-a-Taco service"""
+
     def __init__(self, servDriver, servPlate, servClient, servDepartHour, servArrivalHour, servCircuit, \
                  servCircuitKms, servDriverStatus):
+
+        """Creates a new Service object.
+
+        Requires: servDriver is a srt representing name of a driver. ServPlate is a str representing the plate
+        of a vehicle. servClient is a str representing the client's name. servDepartHour is a str in the "HH:MM" format
+        representing the departing hour of the service. servArrivalHour is a str in the "HH:MM" format
+        representing the arrival hour of the service. servCircuit is a str representing the name of the circuit done by
+        the service. servCircuitKms is a str representing the number of kilometers done in the service. servDriverStatus
+        is a str representing the drivers status. Check the general specification for more information."""
+
         self._servDriver = servDriver
         self._servPlate = servPlate
         self._servClient = servClient
@@ -23,51 +35,115 @@ class Service(object):
         self._servDriverStatus = servDriverStatus
 
     def getServiceDriver(self):
+        """Name of the service driver."""
+
         return self._servDriver
 
     def getServicePlate(self):
+        """Plate of the service vehicle."""
+
         return self._servPlate
 
     def getServiceClient(self):
+        """Name of the service client."""
+
         return self._servClient
 
     def getServiceDepartHour(self):
+        """Service departing hour."""
+
         return self._servDepartHour
 
     def getServiceArrivalHour(self):
+        """Service arrival hour."""
+
         return self._servArrivalHour
 
     def getServiceCircuit(self):
+        """Name of the service circuit."""
+
         return self._servCircuit
 
     def getServiceCircuitKms(self):
+        """Distance of the service, in kilometers."""
+
         return self._servCircuitKms
 
     def getServiceDriverStatus(self):
+        """Status of the driver."""
+
         return self._servDriverStatus
 
     def setServiceDriver(self, servDriver):
+        """Set the name of the service driver.
+
+        Requires: servDriver is a srt representing name of a driver.
+        Ensures: self.getServiceDriver() == servDriver
+        """
+
         self._servDriver = servDriver
 
     def setServicePlate(self, servPlate):
+        """Set the plate of the service vehicle.
+
+        Requires: servPlate is a str reprensenting the vehicle's plate.
+        Ensures: self.getServicePlate() == servPlate
+        """
+
         self._servPlate = servPlate
 
     def setServiceClient(self, servClient):
+        """Set the name of the service client.
+
+        Requires: servClient is a str representing the client name.
+        Ensures: self.getServiceClient() == servClient
+        """
+
         self._servClient = servClient
 
     def setServiceDepartHour(self, servDepartHour):
+        """Set the service departing hour.
+
+        Requires: servDepartHour is a str in the format "HH:MM" representing the departing hour of the service.
+        Ensures: self.getServiceDepartHour() == servDepartHour
+        """
+
         self._servDepartHour = servDepartHour
 
     def setServiceArrivalHour(self, servArrivalHour):
+        """Set the service arrival hour.
+
+        Requires: servArrivalHour is a str in the format "HH:MM" representing the arrival hour of the service.
+        Ensures: self.getServiceArrivalHour(() == servArrivalHour
+        """
+
         self._servArrivalHour = servArrivalHour
 
     def setServiceCircuit(self, servCircuit):
+        """Set the name of the service circuit.
+
+        Requires: servCircuit is a str representing the circuit name.
+        Ensures: self.getServiceCircuit() == servCircuit
+        """
+
         self._servCircuit = servCircuit
 
     def setServiceCircuitKms(self, servCircuitKms):
+        """Set the distance of the service, in kilometers.
+
+        Requires: servCircuitKms is a str representing the distance of the circuit, in kilometers.
+        Ensures: self.getServiceCircuitKms() == servCircuitKms
+        """
+
         self._servCircuitKms = servCircuitKms
 
     def setServiceDriverStatus(self, servDriverStatus):
+        """Set the status of the driver.
+
+        Requires: servDriverStatus is str representing the status of the driver.
+        Ensures: self.getServiceDriverStatus() == servDriverStatus
+        """
+
         self._servDriverStatus = servDriverStatus
 
     def resetVehic(self):
@@ -91,11 +167,15 @@ class Service(object):
         Ensures:
         self depart and arrival hour get changed considering the new reservation.
         """
+        # default delay if no delay
         delay = Time('00:00')
 
+        # if the driver only arrives from the previous service after the client requested hour,
+        # calculate delay.
         if self.getServiceArrivalHour().diff(reservation.getReservRequestedStartHour()) > Time('00:00'):
             delay = self.getServiceArrivalHour().diff(reservation.getReservRequestedStartHour())
 
+        # calculates the depart and arrival hour of the new service, considering the possible delay.
         startHour = reservation.getReservRequestedStartHour().add(delay)
         endHour = reservation.getReservRequestedEndHour().add(delay)
 
