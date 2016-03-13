f122133e8746a197140a14bbc3f9aeed16676931
LLCampos <luis1212@sapo.pt>
2016-03-13 15:27:18 +0000

    add documentation and __str__ method

--- Reservation.py
+++ Reservation.py
@@ -26,36 +26,81 @@ class Reservation(object):
         self._reservCircuitKms = reservCircuitKms
 
     def getReservClient(self):
+        """Name of the client requesting the service."""
+
         return self._reservClient
 
     def getReservRequestedStartHour(self):
+        """The requested start hour of the service."""
+
         return self._reservRequestedStartHour
 
     def getReservRequestedEndHour(self):
+        """The requested end hour of the service."""
+
         return self._reservRequestedEndHour
 
     def getReservCircuit(self):
+        """The requested circuit name."""
+
         return self._reservCircuit
 
     def getReservCircuitKms(self):
+        """The distance, in kms of the requested circuit."""
+
         return self._reservCircuitKms
 
     def setReservClient(self, reservClient):
+        """Sets the name of the client of the reservation.
+
+        Requires: reservClient is a str representing the client name.
+        Ensures: getReservClient() == reservClient  """
+
         self._reservClient = reservClient
 
     def setReservRequestedStartHour(self, reservRequestedStartHour):
+        """Sets the requested start hour of the reservation.
+
+        Requires: reservRequestedStartHour is a Time object representing the requested start hour of the service.
+        Ensures: getReservRequestedStartHour() == reservRequestedStartHour  """
+
         self._reservRequestedStartHour = reservRequestedStartHour
 
     def setReservRequestedEndHour(self, reservRequestedEndHour):
+        """Sets the requested end hour of the reservation.
+
+        Requires: reservRequestedEndHour is a Time object representing the requested end hour of the service.
+        Ensures: getReservRequestedEndHour() == reservRequestedEndHour"""
+
         self._reservRequestedEndHour = reservRequestedEndHour
 
     def setReservCircuit(self, reservCircuit):
+        """Sets the requested circuit name of the reservation.
+
+        Requires: reservCircuit is a str representing the name of the circuit.
+        Ensures: getReservCircuit() == reservCircuit  """
+
         self._reservCircuit = reservCircuit
 
     def setReservCircuitKms(self, reservCircuitKms):
+        """Sets the distance, in kms, of the requested circuit of the reservation.
+
+        Requires: reservCircuitKms is a str representing the distance of the circuit, in kilometers.
+        Ensures: getReservCircuitKms(() == reservCircuitKms"""
+
         self._reservCircuitKms = reservCircuitKms
 
     def duration(self):
         """Calculates the duration of the service requested by this reservation"""
 
         return self.getReservRequestedEndHour().diff(self.getReservRequestedStartHour())
+
+    def __str__(self):
+        """String representation of the reservation"""
+
+        return "Client name:" + self.getReservClient() + "\n" + \
+               "Requested Start Hour: " + self.getReservRequestedStartHour() + "\n" + \
+               "Requested End Hour: " + self.getReservRequestedEndHour() + "\n" + \
+               "Circuit name: " + self.getReservCircuit() + "\n" + \
+               "Distance (in kms): " + self.getReservCircuitKms()
+
